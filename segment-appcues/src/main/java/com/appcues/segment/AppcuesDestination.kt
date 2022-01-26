60fd490f59685ecc4f17b71138cd2ad87dd79e2c
James Ellis <james@appcues.com>
2022-01-26 12:39:40 -0500

    ✨ Map Segment properties/traits into corresponding Appcues SDK function parameters

--- segment-appcues/src/main/java/com/appcues/segment/AppcuesDestination.kt
+++ segment-appcues/src/main/java/com/appcues/segment/AppcuesDestination.kt
@@ -7,7 +7,9 @@ import com.segment.analytics.kotlin.core.platform.DestinationPlugin
 import com.segment.analytics.kotlin.core.platform.Plugin
 import com.segment.analytics.kotlin.core.*
 import com.segment.analytics.kotlin.core.platform.plugins.logger.*
+import com.segment.analytics.kotlin.core.utilities.toContent
 import kotlinx.serialization.*
+import kotlinx.serialization.json.JsonObject
 
 @Serializable
 data class AppcuesSettings(
@@ -26,7 +28,7 @@ class AppcuesDestination(private val context: Context) : DestinationPlugin(), An
             analytics.log("Appcues Destination is enabled")
             val appcuesSettings: AppcuesSettings? = settings.destinationSettings(key)
             if (appcuesSettings != null) {
-                appcues = Appcues.Builder(context, appcuesSettings.appcuesId, "").build()
+                appcues = Appcues.Builder(context, appcuesSettings.appcuesId, "").logging(Appcues.LoggingLevel.BASIC).build()
                 analytics.log("Appcues Destination loaded")
             }
         } else {
@@ -35,22 +37,39 @@ class AppcuesDestination(private val context: Context) : DestinationPlugin(), An
     }
 
     override fun identify(payload: IdentifyEvent): BaseEvent {
-        appcues?.identify(payload.userId, null) //TODO: map props
+        appcues?.identify(payload.userId, payload.traits?.mapToAppcues())
         return payload
     }
 
     override fun track(payload: TrackEvent): BaseEvent {
-        appcues?.track(payload.event, null) //TODO: map props
+        appcues?.track(payload.event, payload.properties?.mapToAppcues())
         return payload
     }
 
     override fun screen(payload: ScreenEvent): BaseEvent {
-        appcues?.screen(payload.name, null) //TODO: map props
+        appcues?.screen(payload.name, payload.properties?.mapToAppcues())
         return payload
     }
 
     override fun group(payload: GroupEvent): BaseEvent {
-        appcues?.group(payload.groupId, null) //TODO: map props
+        appcues?.group(payload.groupId, payload.traits?.mapToAppcues())
         return payload
+    }
+
+    private fun JsonObject.mapToAppcues(): HashMap<String, Any> { // = this.mapValues { (_, value) ->
+        val map: HashMap<String, Any> = hashMapOf()
+        this.forEach { (key, value) ->
+            val newValue = value.toContent()
+            if (newValue != null && isAllowedProperty(newValue)) {
+                map[key] = newValue
+            }
+        }
+        return map
+    }
+
+    private fun isAllowedProperty(value: Any): Boolean {
+        // TODO: will need to evolve as we see what primitive types the underlying SDK
+        // will support in the analytics network traffic - ex: URLs or Dates?
+        return value is String || value is Boolean || value is Int || value is Double
     }
 }
