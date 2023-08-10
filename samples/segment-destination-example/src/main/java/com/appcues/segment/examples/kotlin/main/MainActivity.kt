b97f8309d6147fadbf33aebfd1ee923a445dff16
Matt Hayashida <matt.hayashida@appcues.com>
2023-08-10 10:05:50 -0400

    🎬 Add Appcues URL scheme support to example app

--- samples/segment-destination-example/src/main/java/com/appcues/segment/examples/kotlin/main/MainActivity.kt
+++ samples/segment-destination-example/src/main/java/com/appcues/segment/examples/kotlin/main/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.appcues.segment.examples.kotlin.main
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.appcues.segment.examples.kotlin.ExampleApplication
+import com.appcues.segment.examples.kotlin.ExampleApplication.Companion
 import com.appcues.segment.examples.kotlin.R.id
 import com.appcues.segment.examples.kotlin.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -31,5 +34,20 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        handleLinkIntent(intent)
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        handleLinkIntent(intent)
+    }
+
+    private fun handleLinkIntent(intent: Intent?) {
+        val appcuesHandled = ExampleApplication.appcuesDestination.appcues?.onNewIntent(this, intent) ?: false
+
+        if (appcuesHandled) return
+
+        // otherwise, it was not an Appcues link, the application should handle
     }
 }
