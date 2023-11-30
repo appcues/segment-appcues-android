90bcd4209cb442f756626a98b4e9f2cea49bd264
James Ellis <james@appcues.com>
2023-11-30 16:14:41 -0500

    🎬 Add Debug button in sample app

--- samples/segment-destination-example/src/main/java/com/appcues/segment/examples/kotlin/main/EventsFragment.kt
+++ samples/segment-destination-example/src/main/java/com/appcues/segment/examples/kotlin/main/EventsFragment.kt
@@ -2,10 +2,17 @@ package com.appcues.segment.examples.kotlin.main
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
 import com.appcues.segment.examples.kotlin.ExampleApplication
+import com.appcues.segment.examples.kotlin.R
 import com.appcues.segment.examples.kotlin.databinding.FragmentEventsBinding
 
 class EventsFragment : Fragment() {
@@ -34,6 +41,30 @@ class EventsFragment : Fragment() {
         }
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val menuHost: MenuHost = requireActivity()
+
+        menuHost.addMenuProvider(
+            object : MenuProvider {
+                override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                    menuInflater.inflate(R.menu.events_menu, menu)
+                }
+
+                override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                    return when (menuItem.itemId) {
+                        R.id.events_menu_debug -> {
+                            ExampleApplication.appcuesDestination.appcues?.debug(requireActivity())
+                            true
+                        }
+                        else -> false
+                    }
+                }
+            },
+            viewLifecycleOwner,
+            Lifecycle.State.RESUMED
+        )
     }
 
     override fun onResume() {
